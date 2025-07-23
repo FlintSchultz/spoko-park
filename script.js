bb61eeff7a13587b57436630ad25e30e5270c825
AndreyB <a.balaev@allerhand.ru>
2025-07-23 18:56:19 +0300

    Update layout

--- script.js
+++ script.js
@@ -5,10 +5,61 @@ const btnEn = document.getElementById('en-btn'),
 btnEn.onclick = () => switchLang('en');
 btnPl.onclick = () => switchLang('pl');
 function switchLang(lang) {
-  document.querySelectorAll('[data-en]').forEach(el => el.textContent = el.dataset[lang]);
+  document.querySelectorAll('[data-en]').forEach(el => {
+    const link = el.querySelector('a');
+
+    // Если внутри есть ссылка
+    if (link) {
+      // Политика конфиденциальности
+      if (link.id === 'privacy-policy-link') {
+        const text = lang === 'pl' ? el.dataset.pl : el.dataset.en;
+        const href = link.dataset[`href${lang === 'pl' ? 'Pl' : 'En'}`];
+        link.textContent = text;
+        link.setAttribute('href', href);
+
+      } else {
+        // Текст + ссылка, как "Email us at <a>info@...</a>"
+        const fullText = el.dataset[lang];
+        const linkText = link.textContent;
+
+        // Заменяем всё содержимое элемента, но вставляем <a> заново
+        el.innerHTML = '';
+        const textNode = document.createTextNode(fullText.replace(linkText, '').trim() + ' ');
+        el.appendChild(textNode);
+        el.appendChild(link);
+      }
+
+    } else {
+      // Простой текст
+      el.textContent = el.dataset[lang];
+    }
+  });
   btnEn.classList.toggle('active', lang==='en');
-  btnPl.classList.toggle('active', lang==='pl');
+  btnPl.classList.toggle('active', lang==='pl'); 
 }
+
+document.addEventListener("DOMContentLoaded", function () {
+  // Инициализируем язык по умолчанию
+  const initialLang = document.documentElement.lang || 'en';
+  switchLang(initialLang);
+
+  // Анимация How it works
+    const steps = document.querySelectorAll(".step");
+  const wrapper = document.querySelector(".sticky-wrapper");
+  const vh = window.innerHeight;
+
+  window.addEventListener("scroll", () => {
+    const scrollTop = window.scrollY;
+    const start = wrapper.offsetTop;
+
+    steps.forEach((step, i) => {
+      const triggerPoint = start + i * vh * 1.0;
+      if (scrollTop + vh * 0.6 > triggerPoint) {
+        step.classList.add("visible");
+      }
+    });
+  });
+});
 
 
 // Слайдер
@@ -54,7 +105,7 @@ const observer = new IntersectionObserver(
     });
   },
   {
-    threshold: 0.55 // блок считается видимым, если больше половины видно
+    threshold: 0.15 // блок считается видимым, если больше половины видно
   }
 );
 
